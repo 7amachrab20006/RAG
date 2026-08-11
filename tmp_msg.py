1bdfb32f1ad3a13d44a6f5cede44b11416708f82
mohamed <mohamedbenothmane2006@gmail.com>
2026-08-11 21:09:34 +0100

    Clean up repository and remove temporary files

--- tmp_msg.py
+++
@@ -1,5 +0,0 @@
-﻿try:
-    from langchain.schema import SystemMessage, HumanMessage
-    print('OK')
-except Exception as e:
-    print('ERR', e)
--- tmp_sig.py
+++
@@ -1,5 +0,0 @@
-﻿from inspect import signature, getsource
-from langchain_groq import ChatGroq
-print('signature(generate):', signature(ChatGroq.generate))
-print('signature(invoke):', signature(ChatGroq.invoke))
-print('signature(generate_prompt):', signature(ChatGroq.generate_prompt))
